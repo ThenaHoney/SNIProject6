e2b4d59e255545666d699662aa0c8253e5a59a9b
David Hunt <huntdog1541@gmail.com>
2013-11-22 08:39:24 -0600

    Create disk.h

---
+++ disk.h
@@ -0,0 +1,41 @@
+#ifndef DISK_H_
+#define DISK_H_
+
+/**
+ * Simulates a physical disk. The disk reads and writes blocks of data.
+ *
+ * @author Thomas Reichherzer
+ */
+
+//defines
+/** The size of a block on disk in bytes */
+#define BLOCK_SIZE 1024
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include "inode.h"
+
+typedef struct Block
+{
+	int numOfBlock;
+	char blockData[BLOCK_SIZE];
+	struct Block * nextBlock;
+}block_t;
+
+
+//prototypes
+int createDisk(int sizeInKB, char* path );
+int readBlock(int blockNumber, char* blockData );
+int writeBlock(int blockNumber, char* blockData, int numberOfBytes  );
+int getSize();
+block_t * initializeBlockList(block_t* Block);
+
+//global
+int _sizeInKB;
+int _diskLength;
+FILE* _physicalDisk;
+char errorDesc[256];
+
+#endif /*DISK_H_*/
